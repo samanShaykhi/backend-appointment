6c55ba4b1207fd17997c4c5afc466f1c7a0e40d1
saman <saman@gmail.com>
2026-01-02 14:01:37 +0330

    debug error name url env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongogose = require('mongoose')
 
 const conectToMongose = async () => {
     try {
-        const dbConect = await mongogose.connect(process.env.MONGO_CONECTION)
+        const dbConect = await mongogose.connect(process.env.MONGO_URI)
         console.log(`conect to mongodb :: ${dbConect.connection.host}`)
     } catch (error) {
     }
